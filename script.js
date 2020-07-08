22040069061b7062619172cc5d1603c7d1a95b71
Anh Dang <atd30392@gmail.com>
2020-07-08 17:02:01 -0400

    added smooth scroll

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 $(function() {
-    $('.hamburger').on('click', function() {
-        $('.hamburger').hide();
-        $('header ul').fadeIn();
+    //Show side nav when hamburger clicked
+    $(".hamburger").on("click", function() {
+        $(".hamburger").hide();
+        $("header ul").fadeIn();
     });
-    $('header ul').on('click', function() {
-        $('.hamburger').show();
-        $('header ul').hide();
+
+    // $("header ul").on("click", function() {
+    //     $(".hamburger").show();
+    //     $("header ul").hide();
         
-    });
+    // });
+
+    //Smooth Scroll
+    $("a").on("click", function(e) {
+        e.preventDefault();
+        const value = $(this).attr("value");
+        let target;
+        if (value === "about") {
+            target = ".about";
+        } else if (value === "portfolio") {
+            target = ".portfolio";
+        } else if (value === "contact") {
+            target = ".contact";
+        }
+
+        $("html, body").animate(
+            {
+              scrollTop: $(target).offset().top,
+            },
+            "slow"
+          );
+    })
 
 
 });
